3a286c7689511ec2f70c6ff30231825640910856
uzairnadeem201 <uzairnadeem201@gmail.com>
2023-10-15 22:16:09 +0500

    Assignment2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,8 +35,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -111,35 +113,59 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Quran",
+            "English",
+            "Urdu",
+            "Islamiat",
+            "Pak-Studies"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "REMOVE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // label4
             // 
-            this.label4.AutoEllipsis = true;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(526, 132);
+            this.label4.Location = new System.Drawing.Point(106, 429);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 20);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Result :";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(202, 173);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(159, 20);
-            this.textBox3.TabIndex = 9;
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Result:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1160, 703);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
@@ -165,8 +191,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,9 +15,11 @@ namespace WindowsFormsApp1
     {
         string firstName, lastName, subject;
         bool preRequisited;
+        List<String> subjects;
         public Form1()
         {
             InitializeComponent();
+            this.subjects = new List<String>();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -47,17 +49,78 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            firstName = textBox1.Text;
-            lastName = textBox2.Text;
-            subject = (string)textBox3.Text;
-            preRequisited = checkBox1.Checked;
-            label4.Text = $"First Name : {firstName}\n" + $"Last Name : {lastName}\n" + $"Subject : {subject}\n" + $"preRequisite : {preRequisited}\n";
+            firstName = this.textBox1.Text;
+            lastName = this.textBox2.Text;
+            preRequisited = this.checkBox1.Checked;
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                this.label4.Text = "Please fill all fields";
+                return;
+            }
+            this.label4.Text = $"First Name: {firstName}, Last Name: {lastName},preRequisited: {preRequisited}\n" + $"Subjects Added: ";
+            foreach (string x in this.subjects)
+            {
+              this.label4.Text = this.label4.Text + " , " + x ;
+             
 
+            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String subject;
+            if(this.comboBox1.SelectedItem != null)
+            {
+                subject = this.comboBox1.SelectedItem.ToString();
+                if (subjects.Contains(subject))
+                {
+                    this.label4.Text = $"{subject} is already added";
+                    return;
+                }
+                subjects.Add(subject);
+                this.label4.Text = $"{subject} has been added";
+                
+            }
+            else
+            {
+                this.label4.Text = "Please Select a Subject";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String subject;
+            if (this.comboBox1.SelectedItem != null)
+            {
+                subject = this.comboBox1.SelectedItem.ToString();
+                if (!subjects.Contains(subject))
+                {
+                    this.label4.Text = $"{subject} has not been added yet";
+                    return;
+                }
+                subjects.Remove(subject);
+                this.label4.Text = $"{subject} has been removed";
+
+            }
+            else
+            {
+                this.label4.Text = "Please Select a Subject";
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
